1cb903e938e65fedd8422246729dfcf272c7e8a0
bharat04singh <tarunrajput1526@gmail.com>
2025-05-03 09:13:51 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "bharat@ggoogle.com"
+var accountPassword ="112233"
+
+accountCity = "gurugram"
+
+console.log(accountId);
+
+console.log([accountEmail , accountId ,accountPassword ,]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as new version
+
+//  alert (3 + 3) // we are using nodejs , not browser 
+
+
+console.log(3 
+    +
+3) // code readability should be high 
+
+console.log("bharat")
+
+let name = "bharat"
+let age = 20
+let isLOggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false 
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
